b3792ebfb515ad26ea58416a6684f8b94e2a1fc5
Steven Wagner <leftathome@gmail.com>
2014-10-05 00:58:46 +0200

    Rubocop fixes: String quoting, tabs.

--- test/integration/default/serverspec/apache_installed_and_running_spec.rb
+++ test/integration/default/serverspec/apache_installed_and_running_spec.rb
@@ -1,8 +1,8 @@
 require_relative 'spec_helper'
 
-describe package("apache2") do
-	it { should be_installed }
+describe package('apache2') do
+  it { should be_installed }
 end
 describe port(80) do
-	it { should be_listening }
+  it { should be_listening }
 end
