f8d4ae9b3024364e9d9dd882c5c7e94c12169930
Steven Wagner <leftathome@gmail.com>
2014-10-04 23:44:03 +0200

    Change generator defaults.
    For the record, nothing in this repository is intended to be under
    anything except the MIT license.  It is offered freely in the hope
    that it is useful to others and that others will contribute to it ...

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,14 @@
 name             'chef-koans'
-maintainer       'The Authors'
-maintainer_email 'you@example.com'
-license          'all_rights'
-description      'Installs/Configures chef-koans'
-long_description 'Installs/Configures chef-koans'
+maintainer       'Steven Wagner'
+maintainer_email 'leftathome@gmail.com'
+license          'MIT'
+description      'Matters of great concern should be treated lightly.'
+long_description <<-END
+Matters of small concern should be treated seriously.
+Among one's affairs there should not be more than two or three matters
+of what one could call great concern. If these are deliberated upon
+during ordinary times, they can be understood. Thinking about things
+previously and then handling them lightly when the time comes is what
+this is all about.
+END
 version          '0.1.0'
-
