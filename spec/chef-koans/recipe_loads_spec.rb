6cafbdae98b6f811f559cac52f4374fb6e841022
Steven Wagner <leftathome@gmail.com>
2014-10-07 10:10:09 +0200

    Placate Rubocop.

--- spec/chef-koans/recipe_loads_spec.rb
+++ spec/chef-koans/recipe_loads_spec.rb
@@ -5,10 +5,10 @@ describe 'chef-koans::default' do
     ChefSpec::Runner.new(
       :platform => 'ubuntu',
       :version => '14.04'
-      ).converge(described_recipe)
+    ).converge(described_recipe)
   end
-  it "does not raise errors" do
-        expect(Chef::Formatters::ErrorMapper).to_not receive(:file_load_failed)
-	expect { cb_on_ubuntu1404 }.to_not raise_error
+  it 'does not raise errors' do
+    expect(Chef::Formatters::ErrorMapper).to_not receive(:file_load_failed)
+    expect { cb_on_ubuntu1404 }.to_not raise_error
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 require 'chefspec'
-#require 'chefspec/server'
-#require 'chefspec/berkshelf'
+require 'chefspec/server'
+require 'chefspec/berkshelf'
 
 at_exit { ChefSpec::Coverage.report! }
