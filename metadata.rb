c7071704f96c77a6efef02a35df4c3dc2efa7c02
Steven Wagner <leftathome@gmail.com>
2014-10-07 10:13:18 +0200

    Placate Rubocop.

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
-name             'chef-koans'
-maintainer       'Steven Wagner'
+name 'chef-koans'
+maintainer 'Steven Wagner'
 maintainer_email 'leftathome@gmail.com'
-license          'MIT'
-description      'Matters of great concern should be treated lightly.'
+license 'MIT'
+description 'Matters of great concern should be treated lightly.'
 long_description <<-END
 Matters of small concern should be treated seriously.
 Among one's affairs there should not be more than two or three matters
@@ -11,4 +11,4 @@ during ordinary times, they can be understood. Thinking about things
 previously and then handling them lightly when the time comes is what
 this is all about.
 END
-version          '0.1.0'
+version '0.1.0'
