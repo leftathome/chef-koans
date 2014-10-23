66c66c027842a51cd4e147eac25fea6ad72ca15a
Steven Wagner <leftathome@gmail.com>
2014-10-23 00:01:33 -0700

    Embedding solution, to be removed by Rake during use.

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,6 +8,15 @@
 #
 # The Ubuntu 'apache2' package should be installed.
 
+# SOLUTION START
+package 'apache2'
+# SOLUTION END
 # The Ubuntu 'apache2' service should be started.
+
+# SOLUTION START
+service 'apache2' do
+  action :start
+end
+# SOLUTION END
 
 # Chef will take care of the rest.
